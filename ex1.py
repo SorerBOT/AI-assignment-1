3fdd100c8d237ec3c495e70c913097defb40fd77
SorerBOT <alonfiller@gmaill.com>
2025-12-01 20:28:09 +0200

    (Feat) new datastructures. Did not end up improving anything

--- ex1.py
+++ ex1.py
@@ -14,53 +14,58 @@ def distance(cords_1: tuple[int, int], cords_2: tuple[int, int]):
     return abs(cords_1[0] - cords_2[0]) + abs(cords_1[1] - cords_2[1])
 
 class State:
-    taps:           tuple[int]
-    plants:         tuple[int]
-    robots:         dict[tuple[int, int], tuple[str, int, int]]
-    __hash:         int | None
+    taps:                   tuple[int]
+    plants:                 tuple[int]
+    robots:                 tuple[tuple[str, int, int]]
+    robot_cords:            set[tuple[int, int]]
+    robot_cords_tuple:      tuple[tuple[int, int]]
+    __hash:                 int | None
 
     def __init__(self,
-                _old_state                                  = None,
-                _taps       : tuple[int]            | None  = None,
-                _plants     : tuple[int]            | None  = None,
-                _robots     : dict[tuple, tuple]    | None  = None):
+                _old_state                                                  = None,
+                _taps               : tuple[int]                    | None  = None,
+                _plants             : tuple[int]                    | None  = None,
+                _robot_cords        : set[tuple[int, int]]    | None  = None,
+                _robot_cords_tuple  : tuple[tuple[int, int]]        | None  = None,
+                _robots             : tuple[tuple[str, int, int]]   | None  = None):
 
         if _old_state is not None:
-            self.taps       = _old_state.taps
-            self.plants     = _old_state.plants
-            self.robots     = _old_state.robots
+            self.taps               = _old_state.taps
+            self.plants             = _old_state.plants
+            self.robot_cords        = _old_state.robot_cords
+            self.robot_cords_tuple  = _old_state.robot_cords_tuple
+            self.robots             = _old_state.robots
 
         if _taps is not None:
             self.taps           = _taps
         if _plants is not None:
             self.plants         = _plants
+        if _robot_cords is not None:
+            self.robot_cords    = _robot_cords
+        if _robot_cords_tuple is not None:
+            self.robot_cords_tuple    = _robot_cords_tuple
         if _robots is not None:
-            self.robots         = dict(_robots)
-        self.__hash         = None
+            self.robots         = _robots
 
-    def __str__(self):
-        str_taps                = f"Taps: {self.taps}"
-        str_plants              = f"Plants: {self.plants}"
-        str_robots              = f"Robots: {self.robots}"
-
-        return f"{str_taps}\n{str_plants}\n{str_robots}"
+        self.__hash             = None
 
     def __hash__(self):
-        if self.__hash:
+        if self.__hash is not None:
             return self.__hash
         else:
             self.__hash = hash((
                 self.taps,
                 self.plants,
-                tuple(sorted(self.robots.items())),
-                ))
+                self.robots,
+                self.robot_cords_tuple))
         return self.__hash
 
     def __eq__(self, other):
         return (
             self.taps   == other.taps   and
             self.plants == other.plants and
-            self.robots == other.robots)
+            self.robots == other.robots and
+            self.robot_cords_tuple == other.robot_cords_tuple)
 
 class WateringProblem(search.Problem):
     """This class implements a pressure plate problem"""
@@ -76,13 +81,16 @@ class WateringProblem(search.Problem):
         Don't forget to set the goal or implement the goal test"""
         search.Problem.__init__(self, initial)
         self.heuristics_cache           = {}
-        robots                         = dict(((i,j), (str(id), load, capacity)) for id, (i, j, load, capacity) in initial[KEY_ROBOTS].items())
         self.size                       = initial[KEY_SIZE]
         self.map                        = {}
         plant_values                    = []
         tap_values                      = []
-        self.plant_cords_list           = []
         self.tap_cords_list             = []
+        self.plant_cords_list           = []
+
+        robot_cords_tuple               = tuple((i,j) for id, (i, j, load, capacity) in initial[KEY_ROBOTS].items())
+        robot_cords                     = set(robot_cords_tuple)
+        robots                          = tuple((str(id), load, capacity) for id, (i, j, load, capacity) in initial[KEY_ROBOTS].items())
 
         for index, (plant_cords, plant_water_needed) in enumerate(initial[KEY_PLANTS].items()):
             self.map[plant_cords]   = ("plant", index)
@@ -95,9 +103,11 @@ class WateringProblem(search.Problem):
         for wall_cords in initial[KEY_WALLS]:
             self.map[wall_cords]    = ("wall", -1)
 
-        self.initial                    = State(_taps   = tuple(tap_values),
-                                                _plants = tuple(plant_values),
-                                                _robots = robots)
+        self.initial                = State(_taps               = tuple(tap_values),
+                                            _plants             = tuple(plant_values),
+                                            _robot_cords        = robot_cords,
+                                            _robot_cords_tuple  = robot_cords_tuple,
+                                            _robots             = robots)
 
     def successor(self, state: State):
         """ Generates the successor states returns [(action, achieved_states, ...)]"""
@@ -107,33 +117,26 @@ class WateringProblem(search.Problem):
                 (0 <= i < height)
                 and (0 <= j < width)
                 and self.map.get((i,j), (None, -1))[0] != "wall"
-                and not state.robots.get((i,j), False))
+                and (i,j) not in state.robot_cords)
 
-        # -------------------------------------------------------------------
-        # Didn't want to commit to a specific format for saving the robots. #
-        # -------------------------------------------------------------------
-        generate_robot          = lambda id, i, j, load, capacity: (id, load, capacity)
-        generate_robot_key      = lambda id, i, j, load, capacity: (i,j)
         tuple_replace           = lambda t, index, new_value: t[:index] + (new_value,) + t[index+1:]
 
-        for (i,j), (id, load, capacity) in state.robots.items():
-            old_robot_key           = generate_robot_key(id, i, j, load, capacity)
+        for index, (id, load, capacity) in enumerate(state.robots):
+            (i,j) = state.robot_cords_tuple[index]
 
             if load > 0:
                 (entity_type, entity_index) = self.map.get((i,j), (None, -1))
                 if entity_type == "plant":
                     plant_water_needed = state.plants[entity_index]
                     if plant_water_needed > 0:
-                        state_new_robots    = dict(state.robots)
-                        del state_new_robots[old_robot_key]
-                        state_new_robots[generate_robot_key(id, i, j, load - 1, capacity)] = generate_robot(id, i, j, load - 1, capacity)
+                        state_new_robots        = tuple_replace(state.robots, index, (id, load - 1, capacity))
                         state_new_plants        = tuple_replace(state.plants, entity_index, plant_water_needed - 1)
                         state_new               = State(state,
                                                     _plants = state_new_plants,
                                                     _robots = state_new_robots)
 
                         moves.append((f"POUR{{{id}}}", state_new))
-                        if len(state.robots.items()) == 1:
+                        if len(state.robots) == 1:
                             continue
 
             remaining_capacity = capacity - load
@@ -142,46 +145,40 @@ class WateringProblem(search.Problem):
                 if entity_type == "tap":
                     water_available = state.taps[entity_index]
                     if water_available > 0:
-                            state_new_robots    = dict(state.robots)
-                            del state_new_robots[old_robot_key]
-                            state_new_robots[generate_robot_key(id, i, j, load + 1, capacity)] = generate_robot(id, i, j, load + 1, capacity)
-                            state_new_taps      = tuple_replace(state.taps, entity_index, water_available - 1)
-                            state_new           = State(state,
-                                                    _robots = state_new_robots,
-                                                    _taps   = state_new_taps)
+                        state_new_robots    = tuple_replace(state.robots, index, (id, load + 1, capacity))
+                        state_new_taps      = tuple_replace(state.taps, entity_index, water_available - 1)
+                        state_new           = State(state,
+                                                _robots = state_new_robots,
+                                                _taps   = state_new_taps)
 
-                            moves.append((f"LOAD{{{id}}}", state_new))
-                            if len(state.robots.items()) == 1:
-                                continue
+                        moves.append((f"LOAD{{{id}}}", state_new))
+                        if len(state.robots) == 1:
+                            continue
             if is_move_legal(i+1, j):
-                state_new_robots = dict(state.robots)
-                del state_new_robots[old_robot_key]
-                state_new_robots[generate_robot_key(id, i+1, j, load, capacity)] = generate_robot(id, i+1, j, load, capacity)
-                state_new       = State(state, _robots = state_new_robots)
+                state_new_robot_cords_tuple = tuple_replace(state.robot_cords_tuple, index, (i+1,j))
+                state_new_robot_cords   = set(state_new_robot_cords_tuple)
+                state_new               = State(state, _robot_cords = state_new_robot_cords, _robot_cords_tuple = state_new_robot_cords_tuple)
 
                 moves.append((f"DOWN{{{id}}}", state_new))
 
             if is_move_legal(i-1, j):
-                state_new_robots    = dict(state.robots)
-                del state_new_robots[old_robot_key]
-                state_new_robots[generate_robot_key(id, i-1, j, load, capacity)] = generate_robot(id, i-1, j, load, capacity)
-                state_new           = State(state, _robots = state_new_robots)
+                state_new_robot_cords_tuple = tuple_replace(state.robot_cords_tuple, index, (i-1,j))
+                state_new_robot_cords   = set(state_new_robot_cords_tuple)
+                state_new               = State(state, _robot_cords = state_new_robot_cords, _robot_cords_tuple = state_new_robot_cords_tuple)
 
                 moves.append((f"UP{{{id}}}", state_new))
 
             if is_move_legal(i, j+1):
-                state_new_robots    = dict(state.robots)
-                del state_new_robots[old_robot_key]
-                state_new_robots[generate_robot_key(id, i, j+1, load, capacity)] = generate_robot(id, i, j+1, load, capacity)
-                state_new           = State(state, _robots = state_new_robots)
+                state_new_robot_cords_tuple = tuple_replace(state.robot_cords_tuple, index, (i,j+1))
+                state_new_robot_cords   = set(state_new_robot_cords_tuple)
+                state_new               = State(state, _robot_cords = state_new_robot_cords, _robot_cords_tuple = state_new_robot_cords_tuple)
 
                 moves.append((f"RIGHT{{{id}}}", state_new))
 
             if is_move_legal(i, j-1):
-                state_new_robots    = dict(state.robots)
-                del state_new_robots[old_robot_key]
-                state_new_robots[generate_robot_key(id, i, j-1, load, capacity)] = generate_robot(id, i, j-1, load, capacity)
-                state_new           = State(state, _robots = state_new_robots)
+                state_new_robot_cords_tuple = tuple_replace(state.robot_cords_tuple, index, (i,j-1))
+                state_new_robot_cords   = set(state_new_robot_cords_tuple)
+                state_new               = State(state, _robot_cords = state_new_robot_cords, _robot_cords_tuple = state_new_robot_cords_tuple)
 
                 moves.append((f"LEFT{{{id}}}", state_new))
 
@@ -207,7 +204,8 @@ class WateringProblem(search.Problem):
             return 0
 
         min_robot_contribution_distance         = float('inf')
-        for robot_cords, (id, load, capacity) in node.state.robots.items():
+        for index, (id, load, capacity) in enumerate(node.state.robots):
+            robot_cords = node.state.robot_cords_tuple[index]
             current_robot_contribution_distance = float('inf')
             if load == 0:
                 if non_empty_tap_cords:
@@ -238,7 +236,7 @@ class WateringProblem(search.Problem):
     def h_gbfs(self, node):
         """ This is the heuristic. It gets a node (not a state)
         and returns a goal distance estimate"""
-        return 2 * sum(node.state.plants) - sum(load for (id, load, capacity) in node.state.robots.values())
+        return 2 * sum(node.state.plants) - sum(load for id, load, capacity in node.state.robots)
 
 def create_watering_problem(game):
     print("<<create_watering_problem")
